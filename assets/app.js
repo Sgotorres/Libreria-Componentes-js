1128b40b08cf6874c171cda24bbef89bd44d7bd6
Sgotorres <angelgabrieltorresacevedo98@gmail.com>
2026-05-14 16:15:31 -0400

    chore: estandarización de carpetas de componentes y actualización de rutas

--- assets/app.js
+++ assets/app.js
@@ -37,7 +37,7 @@ const componentData = [
                 </div>
             </div>
         `,
-        source: "src/components/select-dinamico/"
+        source: "src/components/custom-select/"
     },
     {
         name: "Modal",
@@ -65,7 +65,7 @@ const componentData = [
                 </div>
             </div>
         `,
-        source: "src/components/modal/"
+        source: "src/components/custom-modal/"
     },
     {
         name: "Date Range",
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const InputText = require('./components/input-text/index.js');
-const SelectDinamico = require('./components/select-dinamico/index.js');
+const SelectDinamico = require('./components/custom-select/index.js');
 const Gallery = require('./components/gallery/index.js');
-const Modal = require('./components/modal/index.js');
+const Modal = require('./components/custom-modal/index.js');
 const DateRange = require('./components/date-range/index.js');
 
 module.exports = {
